d914b9a30aff3170f24caf4c71f4810b963f3e99
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-18 06:46:59 +0100

    police locks

---
+++ [qb]/qb-doorlock/configs/police.lua
@@ -0,0 +1,436 @@
+
+
+-- 1 created by VybesTV
+Config.DoorList['police-1'] = {
+    locked = false,
+    doors = {
+        {objName = 1308911070, objYaw = 180.00001525879, objCoords = vec3(-582.069702, -409.188904, 35.818611)},
+        {objName = -403433025, objYaw = 180.00001525879, objCoords = vec3(-579.358643, -409.201599, 35.818611)}
+    },
+    doorLabel = '1',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    distance = 2,
+}
+
+-- 2 created by VybesTV
+Config.DoorList['police-2'] = {
+    locked = true,
+    doors = {
+        {objName = -267139712, objYaw = 270.12680053711, objCoords = vec3(-572.819397, -419.203339, 35.390629)},
+        {objName = -267139712, objYaw = 88.978149414062, objCoords = vec3(-572.820557, -421.569214, 35.390629)}
+    },
+    doorLabel = '2',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    distance = 2,
+}
+
+-- 3 created by VybesTV
+Config.DoorList['police-3'] = {
+    locked = true,
+    doors = {
+        {objName = -267139712, objYaw = 0.14265251159668, objCoords = vec3(-567.352661, -433.690308, 35.407803)},
+        {objName = -267139712, objYaw = 179.00653076172, objCoords = vec3(-564.986511, -433.690308, 35.407803)}
+    },
+    doorLabel = '3',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    distance = 2,
+}
+
+-- 4 created by VybesTV
+Config.DoorList['police-4'] = {
+    locked = true,
+    doors = {
+        {objName = 1308911070, objYaw = 0.14265251159668, objCoords = vec3(-569.587219, -439.537872, 35.818611)},
+        {objName = -403433025, objYaw = 0.0, objCoords = vec3(-572.298889, -439.544342, 35.818611)}
+    },
+    doorLabel = '4',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    distance = 2,
+}
+
+-- 5 created by VybesTV
+Config.DoorList['police-5'] = {
+    locked = true,
+    objYaw = 359.07043457031,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-587.513977, -416.079132, 35.146538),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '5',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 6 created by VybesTV
+Config.DoorList['police-6'] = {
+    locked = true,
+    objYaw = 359.07043457031,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-587.497192, -428.232056, 35.144337),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '6',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 7 created by VybesTV
+Config.DoorList['police-7'] = {
+    locked = false,
+    cantUnlock = true,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-588.615967, -417.858124, 35.144688),
+    objName = -1498371782,
+    fixText = false,
+    objYaw = 269.51995849609,
+    doorLabel = '7',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 8 created by VybesTV
+Config.DoorList['police-8'] = {
+    locked = true,
+    objYaw = 359.07043457031,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-590.632263, -419.524567, 35.142044),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '8',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 9 created by VybesTV
+Config.DoorList['police-9'] = {
+    locked = true,
+    objYaw = 90.294982910156,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-588.665955, -439.325500, 35.144341),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '9',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 10 created by VybesTV
+Config.DoorList['police-10'] = {
+    locked = true,
+    objYaw = 89.999977111816,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-588.601501, -439.315308, 31.141899),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '10',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 11 created by VybesTV
+Config.DoorList['police-11'] = {
+    locked = true,
+    objYaw = 90.018165588379,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-530.926758, -412.072449, 35.639088),
+    objName = 647187752,
+    fixText = false,
+    doorLabel = '11',
+    doorType = 'garage',
+    doorRate = 1.0,
+    distance = 6,
+}
+
+-- 12 created by VybesTV
+Config.DoorList['police-12'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-588.468323, -443.446655, 31.264170),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '12',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 13 created by VybesTV
+Config.DoorList['police-13'] = {
+    locked = true,
+    objYaw = 269.87484741211,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-585.852234, -443.592773, 31.316887),
+    objName = 749848321,
+    fixText = false,
+    doorLabel = '13',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 14 created by VybesTV
+Config.DoorList['police-14'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-585.842651, -449.293396, 31.273935),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '14',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 15 created by VybesTV
+Config.DoorList['police-15'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-588.481201, -449.342987, 31.264040),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '15',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 16 created by VybesTV
+Config.DoorList['police-16'] = {
+    locked = true,
+    objYaw = 269.87484741211,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-572.992615, -443.620605, 31.316887),
+    objName = 749848321,
+    fixText = false,
+    doorLabel = '16',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 17 created by VybesTV
+Config.DoorList['police-17'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-575.620239, -443.465637, 31.273935),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '17',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 18 created by VybesTV
+Config.DoorList['police-18'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-572.997253, -449.311371, 31.258741),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '18',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 19 created by VybesTV
+Config.DoorList['police-19'] = {
+    locked = true,
+    objYaw = 89.874816894531,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-575.662720, -449.353912, 31.277338),
+    objName = -1498371782,
+    fixText = false,
+    doorLabel = '19',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+}
+
+-- 20 created by VybesTV
+Config.DoorList['police-20'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-546.690796, -430.779419, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '20',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 21 created by VybesTV
+Config.DoorList['police-21'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-546.715881, -434.147461, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '21',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 22 created by VybesTV
+Config.DoorList['police-22'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-550.120300, -434.122131, 31.314978),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '22',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 23 created by VybesTV
+Config.DoorList['police-23'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-550.097778, -430.754059, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '23',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 24 created by VybesTV
+Config.DoorList['police-24'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-560.255737, -434.148010, 31.314978),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '24',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 25 created by VybesTV
+Config.DoorList['police-25'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-560.237244, -430.909271, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '25',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 26 created by VybesTV
+Config.DoorList['police-26'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-563.645325, -434.122803, 31.314978),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '26',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 27 created by VybesTV
+Config.DoorList['police-27'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-563.604431, -430.933533, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '27',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 28 created by VybesTV
+Config.DoorList['police-28'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-567.109863, -434.097015, 31.314978),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '28',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 29 created by VybesTV
+Config.DoorList['police-29'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-567.074890, -430.907715, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '29',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 30 created by VybesTV
+Config.DoorList['police-30'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-570.609741, -434.102234, 31.314978),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '30',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- 31 created by VybesTV
+Config.DoorList['police-31'] = {
+    locked = true,
+    objYaw = 359.57388305664,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(-570.584351, -430.881592, 31.319519),
+    objName = -557732756,
+    fixText = false,
+    doorLabel = '31',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+}
