5a6a4b6d896dca418deb9f7cbab5f1a4e1eedf4e
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-18 06:51:56 +0100

    newphone

---
+++ [scripts]/[lb]/lb-phone/fxmanifest.lua
@@ -0,0 +1,55 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+version "2.1.2"
+
+shared_script {
+    "config/*.lua",
+    "shared/**/*.lua"
+}
+
+client_script "client/**/*.lua"
+
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "server/**/*.lua",
+}
+
+files {
+    "ui/dist/**/*",
+    "ui/components.js",
+    "config/**/*"
+}
+
+ui_page "ui/dist/index.html"
+
+dependencies {
+    "loaf_lib",
+    "oxmysql"
+}
+
+escrow_ignore {
+    "config/**/*",
+
+    "client/apps/framework/**/*.lua",
+    "server/apps/framework/**/*.lua",
+    "shared/*.lua",
+
+    "client/custom/**/*.lua",
+    "server/custom/**/*.lua",
+
+    "client/misc/debug.lua",
+    "server/misc/debug.lua",
+
+    "server/misc/functions.lua",
+    "server/misc/databaseChecker/*.lua",
+
+    "server/apiKeys.lua",
+
+    "types.lua",
+
+    "client/apps/default/weather.lua",
+}
+
+dependency '/assetpacks'
