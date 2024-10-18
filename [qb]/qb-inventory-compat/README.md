496f121bc66d44a23e8dfe02d496e8e19a20fef9
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-18 09:13:40 +0100

    temp fix for inventory

---
+++ [qb]/qb-inventory-compat/README.md
@@ -0,0 +1,7 @@
+THIS IS NOT AN INVENTORY ITS AN ADDON FOR THE INVENTORY!!!
+
+This adds the exploitable parts back into the inventory and isnt reccomended for production use, but it ultimately adds complete backward compatibility to the old qb-inventory event.
+
+DRAG AND DROP...
+
+Then update your inventory.
---
+++ [qb]/qb-inventory-compat/server.lua
@@ -0,0 +1,12 @@
+RegisterServerEvent("inventory:server:OpenInventory", function(_type, inventoryName, exploitableItemsList)
+    if _type == "shop" then
+        exports['qb-inventory']:CreateShop({
+            name = inventoryName,
+            label = exploitableItemsList.label,
+            slots = #exploitableItemsList.items,
+            items = exploitableItemsList.items
+        })
+        return exports['qb-inventory']:OpenShop(source, inventoryName)
+    end
+    exports['qb-inventory']:OpenInventory(source, inventoryName)
+end)
