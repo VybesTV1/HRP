573801adcbe06df4c973045cbd73b591433fe6fc
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-18 05:18:33 +0100

    mlo's

---
+++ [defaultmaps]/[prison]/cfx-gabz-prison/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Gabz'
+description 'Prison'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+dependencies {
+    '/server:4960',     -- ⚠️PLEASE READ⚠️; Requires at least SERVER build 4960.
+    '/gameBuild:2545',  -- ⚠️PLEASE READ⚠️; Requires at least GAME build 2545.
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+    'prison.lua',
+}
+dependency '/assetpacks'
