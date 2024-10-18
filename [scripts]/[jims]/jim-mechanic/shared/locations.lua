1a305ca3d370adcf5f2913f90107715d41d745d6
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-18 08:00:54 +0100

    Update locations.lua

--- [scripts]/[jims]/jim-mechanic/shared/locations.lua
+++ [scripts]/[jims]/jim-mechanic/shared/locations.lua
@@ -4,43 +4,34 @@ Config.Locations[#Config.Locations+1] = { --[[ GABZ ALTA STREET BENNYS ]]--
 	Enabled = true,
 	job = "mechanic",
 	zones = {
-		vec2(-263.99075317382, -1349.6701660156),
-		vec2(-263.5015258789, -1298.9702148438),
-		vec2(-229.94024658204, -1299.089477539),
-		vec2(-229.81985473632, -1291.589477539),
-		vec2(-216.73846435546, -1288.9470214844),
-		vec2(-193.63221740722, -1294.155883789),
-		vec2(-174.24346923828, -1293.1431884766),
-		vec2(-151.77659606934, -1300.6693115234),
-		vec2(-151.88639831542, -1311.1921386718),
-		vec2(-177.41833496094, -1311.566772461),
-		vec2(-177.5919342041, -1351.1942138672)
+		vec2(2765.5346679688, 3506.3024902344),
+		vec2(2687.0441894531, 3540.8635253906),
+		vec2(2671.93359375,   3505.4375),
+		vec2(2657.2563476562, 3511.9497070312),
+		vec2(2643.5556640625, 3473.9645996094),
+		vec2(2735.0197753906, 3433.0856933594)
 	},
-	autoClock = { enter = true, exit = true, },
+	autoClock = { enter = false, exit = false, },
 	stash = {
-		{ coords = vec4(-226.48, -1316.17, 31.27, 0.0), w = 3.6, d = 0.8, },
+		{ coords = vec4(2707.59, 3497.63, 55.97, 63.54), w = 3.6, d = 0.8, },
 	},
 	store = {
 		{ coords = vec4(-228.64, -1314.19, 31.3, 90.0), w = 3.60, d = 0.8 },
 	},
 	crafting = {
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
-		{ coords = vec4(-214.82, -1339.74, 31.46, 90.0), w = 2.8, d = 1.5 },
+		{ coords = vec4(2713.06, 3477.29, 55.67, 66.63), w = 2.8, d = 1.5 },
+		{ coords = vec4(2715.24, 3482.83, 55.67, 69.08), w = 2.8, d = 1.5 },
+		{ coords = vec4(2717.67, 3488.47, 55.67, 66.51), w = 2.8, d = 1.5 },
+		{ coords = vec4(2721.26, 3497.06, 55.67, 43.96), w = 2.8, d = 1.5 },
+		{ coords = vec4(2723.61, 3502.71, 55.67, 64.28), w = 2.8, d = 1.5 },
+		{ coords = vec4(2725.77, 3507.81, 55.67, 66.62), w = 2.8, d = 1.5 },
 	},
 	clockin = {
-		{ coords = vec4(-195.55, -1316.46, 31.2, 181.72), prop = false },
+		{ coords = vec4(2716.53, 3457.94, 55.67, 312.47), prop = false },
 	},
 	manualRepair = {
 		{ coords = vec4(-200.28, -1311.62, 31.3, 0.0), prop = true, },
 	},
-	carLift = {
-        { coords = vec4(-201.85, -1319.65, 31.3, 19.36), useMLOLift = true },
-        { coords = vec4(-221.27, -1318.71, 31.3, 352.67), useMLOLift = true },
-    },
 	garage = {
 		spawn = vec4(-182.74, -1317.61, 30.63, 357.23),
 		out = vec4(-190.62, -1311.57, 31.3, 0.0),
@@ -49,7 +40,7 @@ Config.Locations[#Config.Locations+1] = { --[[ GABZ ALTA STREET BENNYS ]]--
 	},
 	payments = {
 		img = "https://static.wikia.nocookie.net/gtawiki/images/b/be/BennysOriginalMotorWorks-GTAO-Logo.png",
-		{ coords = vec4(-192.21, -1316.34, 31.10, 285.83), prop = true },
+		{ coords = vec4(2727.46, 3457.17, 55.67, 68.38), prop = true },
 	},
 	Restrictions = {
 		Vehicle = { "Compacts", "Sedans", "SUVs", "Coupes", "Muscle", "Sports Classics", "Sports", "Super", "Motorcycles", "Off-road", "Industrial", "Utility", "Vans", "Cycles", "Service", "Emergency", "Commercial", },
