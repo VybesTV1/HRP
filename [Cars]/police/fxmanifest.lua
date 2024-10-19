c607ed780f8e9dd34630ea7c9e17a56c124d46ea
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-19 19:59:53 +0100

    cars fixed

--- [Cars]/police/fxmanifest.lua
+++ [Cars]/police/fxmanifest.lua
@@ -2,15 +2,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'PLOKMJNB#8692'
-description 'A single resource car pack to eliminate the rest'
-repository 'https://github.com/PLOKMJNB/FiveM-Civ-Car-Pack'
-
 files {
     'data/**/*.meta',
-    'data/**/*.xml',
-    'data/**/*.dat',
-    'data/**/*.ytyp'
 }
 
 
@@ -21,9 +14,3 @@ data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
 data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
 data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
-
-client_scripts {
-    'vehicle_names.lua',
-}
-
-server_script 'server.lua'
