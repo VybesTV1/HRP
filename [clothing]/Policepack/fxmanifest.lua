2f8439c488881ca2131c796bad959cb1de16ede3
Miz-coder <hi.89misery@gmail.com>
2024-10-19 14:37:09 +0100

    Hate clothing

--- [clothing]/Policepack/fxmanifest.lua
+++ [clothing]/Policepack/fxmanifest.lua
@@ -4,9 +4,9 @@ fx_version 'bodacious'
 game { 'gta5' }
 
 files {
-  'mp_m_freemode_01_mp_m_Police.meta',
-  'mp_f_freemode_01_mp_f_Police.meta'
+  'mp_m_freemode_01_mp_m_police.meta',
+  'mp_f_freemode_01_mp_f_police.meta'
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_Police.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_Police.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_police.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_police.meta'
