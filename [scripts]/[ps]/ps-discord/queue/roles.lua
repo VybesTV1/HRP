3bc97395ee0c9317598f92e6a23f8a9f0db55b91
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-21 08:04:49 +0100

    update

---
+++ [scripts]/[ps]/ps-discord/queue/roles.lua
@@ -0,0 +1,14 @@
+-- Role(s) that are allowed to go through the queue, you may leave it empty if you want everyone in your server to be able to join
+local allowlistedRoles = {
+    "1267940573420064778"
+}
+
+-- Put in what priority their Discord role should have, the higher they are on the list the higher priority they have.
+local priorityRoles = {
+    "1267940562607280128"
+}
+
+return {
+    allowlistedRoles = allowlistedRoles,
+    priorityRoles = priorityRoles
+}
