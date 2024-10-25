c76320209c4bb77d8ae27048e40e69cdbe1d73c0
VybesTV <155245988+VybesTV1@users.noreply.github.com>
2024-10-25 05:56:03 +0100

    scooter rental added

---
+++ [qb]/qb-scooterrental/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.PedModel = 'a_m_m_prolhost_01'
+Config.PedLocation = vector4(-1041.00, -2729.86, 10.80, 233.59)
+Config.SpawnLocation = vector4(-1036.96, -2726.70, 13.76, 301.04)
+
+Config.ScooterModel = "gcscoot"
+Config.ScooterPrice = 50
+
+
+Config.FuelSystem = "cdn-fuel"
